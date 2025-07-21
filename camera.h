8cce572d7f7ef2b1a401fb08240df24f8ade3d29
FelipeKenxi <felipemelo@estudante.ufscar.br>
2025-07-21 15:34:27 -0300

    Céu noturno no camera.h
    Muda o gradiente de cor de fundo, dando uma cor mais semelhante à um céu noturno.

--- camera.h
+++ camera.h
@@ -130,7 +130,7 @@ class camera {
 
         vec3 unit_direction = unit_vector(r.direction());
         auto a = 0.5*(unit_direction.y() + 1.0);
-        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        return (1.0-a)*color(0.0, 0.0, 0.05) + a * color(0.02, 0.05, 0.2); //Felipe P Melo, céu noturno
     }
 };
 
